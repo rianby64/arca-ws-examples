557059a1b314a8f021461d601ae47239c94d9130
Andres Rios <Andres_Rios@epam.com>
2018-11-25 16:07:56 +0300

    Arrange the context processing

--- arca/process.go
+++ arca/process.go
@@ -7,9 +7,36 @@ import (
 )
 
 func processJSONRPCrequest(request *JSONRPCrequest, conn *websocket.Conn) {
-	handler, ok := handlers["Users"][request.Method]
+	if request.Context == nil {
+		log.Println("Context must be present in request", request)
+		return
+	}
+	contextRequest, ok := request.Context.(map[string]interface{})
 	if !ok {
-		log.Println("There's no handler for", request.Method)
+		log.Println("Context must be an Object", request)
+		return
+	}
+	if contextRequest["source"] == nil {
+		log.Println("Context must define a source", request)
+		return
+	}
+	sourceRequest, ok := contextRequest["source"].(string)
+	if !ok {
+		log.Println("Context has an incorrect source",
+			contextRequest, "expecting an string")
+		return
+	}
+
+	source, ok := handlers[sourceRequest]
+	if !ok {
+		log.Printf("source not found for '%s'", sourceRequest)
+		return
+	}
+
+	handler, ok := source[request.Method]
+	if !ok {
+		log.Printf("handler not found for '%s' in source '%s'",
+			request.Method, sourceRequest)
 		return
 	}
 	result, err := handler(&request.Params, &request.Context)
