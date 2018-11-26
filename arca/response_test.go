d944a33dd5b4b0f998f381cf933b9af54478745c
Andres Rios <Andres_Rios@epam.com>
2018-11-26 22:19:53 +0300

    Improve last test

--- arca/response_test.go
+++ arca/response_test.go
@@ -7,6 +7,28 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+func checkRequestResponse(t *testing.T, result *interface{},
+	request *JSONRPCrequest, response *JSONRPCresponse) {
+	if response.Context == &request.Context {
+		t.Log("Reflect context from response")
+	} else {
+		t.Log(response.Context, request.Context)
+		t.Error("Context must be reflected")
+	}
+	if response.Method == request.Method {
+		t.Log(response.Method, request.Method)
+		t.Log("Reflect method from response")
+	} else {
+		t.Error("Method must be reflected")
+	}
+	if response.Result == result {
+		t.Log("Reflect result from response")
+	} else {
+		t.Log(response.Result, result)
+		t.Error("Result must be reflected")
+	}
+}
+
 func Test_response_one_request_with_ID(t *testing.T) {
 	request := JSONRPCrequest{}
 	request.ID = "ID"
@@ -18,30 +40,23 @@ func Test_response_one_request_with_ID(t *testing.T) {
 	counter := 0
 
 	writeJSON = func(conn *websocket.Conn, response *JSONRPCresponse) error {
-		if response.Context == &request.Context {
-			t.Log("Reflect context from response")
-		} else {
-			t.Error("Context must be reflected")
-		}
-		if response.Method == request.Method {
-			t.Log("Reflect method from response")
-		} else {
-			t.Error("Method must be reflected")
-		}
-		if response.Result == &result {
-			t.Log("Reflect result from response")
-		} else {
-			t.Error("Result must be reflected")
-		}
+		checkRequestResponse(t, &result, &request, response)
 		counter++
 		return nil
 	}
 
-	response(&request, &conn, &result)
+	errs := response(&request, &conn, &result)
 	if counter == 1 {
 		t.Log("One response was send")
 	} else {
 		t.Error("No response was send")
+	}
+	if len(errs) == 1 {
+		if errs[0] == nil {
+			t.Log("The response was send correctly")
+		} else {
+			t.Error(errs)
+		}
 	}
 	setupGlobals()
 }
