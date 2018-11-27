5569048de52e4cb119782879d067a30fe9c8630e
Andres Rios <Andres_Rios@epam.com>
2018-11-27 14:13:16 +0300

    Implement the test when matching a handler

--- arca/process_test.go
+++ arca/process_test.go
@@ -1,6 +1,7 @@
 package arca
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/gorilla/websocket"
@@ -20,6 +21,7 @@ func Test_matchHandler_request_without_Method(t *testing.T) {
 	} else {
 		t.Error("a Method must be defined at Handler")
 	}
+	setupGlobals()
 }
 
 func Test_matchHandler_request_without_Context(t *testing.T) {
@@ -37,6 +39,7 @@ func Test_matchHandler_request_without_Context(t *testing.T) {
 	} else {
 		t.Error("a Context must be defined at Handler")
 	}
+	setupGlobals()
 }
 
 func Test_matchHandler_request_with_incorrect_Context(t *testing.T) {
@@ -55,6 +58,7 @@ func Test_matchHandler_request_with_incorrect_Context(t *testing.T) {
 	} else {
 		t.Error("a Context must be defined at Handler")
 	}
+	setupGlobals()
 }
 
 func Test_matchHandler_request_with_Context_without_source(t *testing.T) {
@@ -73,4 +77,40 @@ func Test_matchHandler_request_with_Context_without_source(t *testing.T) {
 	} else {
 		t.Error("The Context must contain a source")
 	}
+	setupGlobals()
+}
+
+func Test_matchHandler_request_with_Context_with_source(t *testing.T) {
+	t.Log("Match a handler fails if context defined in request contains a source")
+	conn := websocket.Conn{}
+	methods := DIRUD{
+		Read: func(requestParams *interface{},
+			context *interface{}) (interface{}, error) {
+			return nil, nil
+		},
+	}
+	RegisterSource("source-defined", &methods)
+	request := JSONRPCrequest{}
+	request.Method = "read"
+	request.Context = map[string]interface{}{"source": "source-defined"}
+
+	handler, err := matchHandler(&request, &conn)
+	if err != nil {
+		t.Error("Unexpected error", err)
+	}
+	if handler == nil {
+		t.Error("The Context must match the handler [source-defined][read]")
+		if err == nil {
+			t.Error("nil handler must lead to an error")
+		}
+	} else {
+		var ptHandler = reflect.ValueOf(*handler).Pointer()
+		var ptMethod = reflect.ValueOf(methods.Read).Pointer()
+		if ptHandler == ptMethod {
+			t.Log("Matched handler and given method are the same")
+		} else {
+			t.Error("Matched handler differs from given method")
+		}
+	}
+	setupGlobals()
 }
