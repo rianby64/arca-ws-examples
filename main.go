a0903538aca6d9b0bad775a3a39a5081c97b8f34
Andres Rios <rianby64@gmail.com>
2018-12-15 17:21:48 +0300

    Comience desde ceros nuevamente

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"./arca"
-	"./examples"
 )
 
 func main() {
 
-	examples.Start()
-	http.HandleFunc("/ws", arca.Handle)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	err := http.ListenAndServe(":8080", nil)
